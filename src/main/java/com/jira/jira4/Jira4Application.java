1ee9e2b8ef877bd3272ecc0d6d71f62ebe2c64e2
romaRacoon <roma1808@inbox.ru>
2023-10-07 20:20:09 +0300

    init

---
+++ src/main/java/com/jira/jira4/Jira4Application.java
@@ -0,0 +1,13 @@
+package com.jira.jira4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jira4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jira4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jira/jira4/Jira4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jira.jira4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jira4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
