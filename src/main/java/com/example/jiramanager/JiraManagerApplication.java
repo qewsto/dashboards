f237c5ca355e5f17eaed0d8bcd06a83ba53d905b
romaRacoon <roma1808@inbox.ru>
2023-10-07 21:03:51 +0300

    init

---
+++ src/main/java/com/example/jiramanager/JiraManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jiramanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jiramanager/JiraManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jiramanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
